ac8bd330c9d206d9791350f7cf4b035752a2e2fe
Aleksandr Samokhin <alexsamohin@gmail.com>
2022-06-19 19:27:59 +0300

    Added translate app

--- sm/firm/views.py
+++ sm/firm/views.py
@@ -7,7 +7,5 @@ from django.views.generic.base import TemplateView
 class ImportView(TemplateView):
 
     def get(self, request, *args, **kwargs):
-        #file_name = '/home/sam/Загрузки/Telegram Desktop/ChatExport_2022-05-15/result.json'
-        #chat_name = import_json_chat(file_name)
         management.call_command('compilemessages', )
         return HttpResponse('Chat ' + 'chat_name')
