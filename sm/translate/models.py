d09f2466f074381b979816a8e9879b112440dd34
Aleksandr Samokhin <alexsamohin@gmail.com>
2022-06-20 02:11:06 +0300

    Added translate model

--- sm/translate/models.py
+++ sm/translate/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class phrase(models.Model):
+    ru = models.CharField(max_length=80, verbose_name='Ru', help_text='Русский')
+    en = models.CharField(max_length=80, verbose_name='En', help_text='English')
+
+    def __str__(self):
+        return self.ru
+
+    class Meta:
+        ordering = ['ru']
+        verbose_name = 'Перевод'
+        verbose_name_plural = 'Переводы'
