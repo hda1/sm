7f7467c87a57b99726368a9bab586b539586a544
Aleksandr Samokhin <alexsamohin@gmail.com>
2022-06-26 18:47:16 +0300

    Added model Position to ad

--- sm/ad/models.py
+++ sm/ad/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    title = models.CharField(max_length=75, verbose_name='Расположение')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Расположение'
+        verbose_name_plural = 'Расположения'
