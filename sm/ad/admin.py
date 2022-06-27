49aa4a00d4c38d6b8fc4cd91f888237acfc70264
Aleksandr Samokhin <alexsamohin@gmail.com>
2022-06-27 22:02:26 +0300

    Added ad.Position to admin.py

--- sm/ad/admin.py
+++ sm/ad/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+
+
+admin.site.register(Position, PositionAdmin)
