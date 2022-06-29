a29086fda86d8817958c7dbc95fcb0512b0d37be
Aleksandr Samokhin <alexsamohin@gmail.com>
2022-06-29 21:04:51 +0300

    Added ad.Language

--- sm/ad/models.py
+++ sm/ad/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
+class Language(models.Model):
+    title = models.CharField(max_length=75, verbose_name='Язык')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Язык'
+        verbose_name_plural = 'Языки'
+
+
 class Position(models.Model):
     title = models.CharField(max_length=75, verbose_name='Расположение')
 
